358b340faf25b27353a23369d986252cdfb1b9bb
Don Leistman <donleistman@gmail.com>
2018-03-05 17:59:34 -0500

    end of day

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require( 'express' );
+const volleyball = require('volleyball');
 const app = express(); // creates an instance of an express application
 
 app.listen(3000, () => console.log('server listening'));
 
+app.use(volleyball);
+
+app.use('/special', (req, res, next) => {
+  console.log(req.method, req.path);
+  console.log('you found the special thing');
+  next();
+});
+
 app.get('/', (req, res, next) => {
   res.send('Hello, world!');
-})
+});
